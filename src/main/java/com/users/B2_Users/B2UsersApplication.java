730b57b4b7d85bc7e38bfd8f3d442ea6896c134a
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-20 14:57:36 +0530

    spring data jpa project on spring boot.

---
+++ src/main/java/com/users/B2_Users/B2UsersApplication.java
@@ -0,0 +1,13 @@
+package com.users.B2_Users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2UsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/users/B2_Users/B2UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.B2_Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
